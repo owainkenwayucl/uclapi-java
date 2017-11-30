7f0b8da17e5ee476c60b47ffaa93f74f32f467e4
Owain Kenway <o.kenway@ucl.ac.uk>
2017-11-30 12:54:23 +0000

    First attempt at a booking class.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 JAVAC = javac 
 
-all: uclapi/UCLApiConnection.class uclapi/Room.class uclapi/Person.class uclapi/Equipment.class
+all: uclapi/UCLApiConnection.class uclapi/Room.class uclapi/Person.class uclapi/Equipment.class uclapi/Booking.class
 
 uclapi/UCLApiConnection.class: uclapi/UCLApiConnection.java Makefile
 	$(JAVAC) uclapi/UCLApiConnection.java
@@ -13,6 +13,9 @@ uclapi/Person.class: uclapi/Person.java Makefile
 
 uclapi/Equipment.class: uclapi/Equipment.java Makefile
 	$(JAVAC) uclapi/Equipment.java
+
+uclapi/Booking.class: uclapi/Booking.java Makefile
+	$(JAVAC) uclapi/Booking.java
 
 clean:
 	rm -rf uclapi/*.class *.class trash/*.class examples/*.class
---
+++ uclapi/Booking.java
@@ -0,0 +1,299 @@
+/**
+ * This class represents a room booking in the UCL API.
+ * @author Owain Kenway
+ */
+package uclapi;
+
+import uclapi.UCLApiConnection;
+import uclapi.Room;
+
+import java.lang.Math;
+import java.util.Hashtable;
+import java.util.LinkedList;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
+
+public class Booking {
+     
+    // Extracted from JSON example.
+    
+    private String roomname;
+    private String roomid;
+    private String siteid;
+
+    private String endtime;
+    private String starttime;
+    private long slotid;
+    private String phone;
+    private String description;
+    private double weeknumber;
+    private String contact;
+
+
+    /**
+     * Constructor to create a blank room.
+     */
+    public Booking() {
+        this.roomname = "";
+        this.roomid = "";
+        this.siteid = "";
+        this.endtime = "";
+        this.starttime = "";
+        this.slotid = 0;
+        this.phone = "";
+        this.description = "";
+        this.weeknumber = 0d;
+        this.contact = "";
+    }
+
+    /**
+     * Use JSON.simple to create a booking from a JSONObject.
+     * NOTE: Assumes the object IS a booking!
+     * @param jsonbooking a JSONObject containing a booking.
+     */
+    public Booking(JSONObject jsonbooking) {
+        try {
+            this.roomname = new String(((String)jsonbooking.get("roomname")));
+            this.roomid = new String(((String)jsonbooking.get("roomid")));
+            this.siteid = new String(((String)jsonbooking.get("siteid")));
+            this.endtime = new String(((String)jsonbooking.get("end_time")));
+            this.starttime = new String(((String)jsonbooking.get("start_time")));
+            this.phone = new String(((String)jsonbooking.get("phone")));
+            this.description = new String(((String)jsonbooking.get("description")));
+            this.contact = new String(((String)jsonbooking.get("contact")));
+            this.slotid = ((long)jsonbooking.get("slotid"));            
+            this.weeknumber = Double.parseDouble((String)jsonbooking.get("weeknumber"));
+
+        } catch(Exception e) {
+            System.err.println(e.toString());
+            System.exit(2);
+        }
+
+    }
+
+    /**
+     * Perform a query on a given UCL API connection and return an array of bookings.
+     * @param conn UCLApiConnection
+     * @param params hashtable of query parameters
+     * @return Array of bookings
+     */
+    public static Booking[] searchAPI(UCLApiConnection conn, Hashtable<String, String> params) {
+        return Booking.searchAPI(conn, UCLApiConnection.RoomBookingsEP, params);
+    }
+
+    /**
+     * Perform a query on a given UCL API connection and return an array of bookings.
+     * @param conn UCLApiConnection
+     * @param endpoint the API path
+     * @param params hashtable of query parameters
+     * @return Array of bookings
+     */
+    public static Booking[] searchAPI(UCLApiConnection conn, String endpoint, Hashtable<String, String> params) {
+        String response = conn.queryAPI(endpoint, params);
+
+        try {
+            boolean cont = true;
+            LinkedList<Booking> bookings = new LinkedList<Booking>();
+
+            while (cont) {
+                JSONParser p = new JSONParser();
+                JSONObject responseObject = (JSONObject)p.parse(response);
+
+                JSONArray bs = (JSONArray)responseObject.get("bookings");
+
+                int nBookings = bs.size();
+                
+                for (int i = 0; i < nBookings; i++) {
+                    bookings.add(new Booking((JSONObject)bs.get(i)));
+                }
+
+                cont = Boolean.parseBoolean((String)responseObject.get("next_page_exists"));
+                params.put("page_token",(String)responseObject.get("page_token"));
+            }
+            return (Booking[])bookings.toArray();
+
+        } catch (Exception e){
+            System.err.println(e.toString());
+            System.exit(5);
+        }
+        return new Booking[0];
+    }
+    /**
+     * @return a string representation of the room.
+     */
+    public String toString() {
+        String retval = "";
+        retval = retval + "Name: " + this.roomname + "\n"
+                        + "Room id: " + this.roomid + "\n"
+                        + "Site id: " + this.siteid + "\n"
+                        + "Description: " + this.description + "\n"
+                        + "Start: " + this.starttime + "\n"
+                        + "End: " + this.endtime + "\n"
+                        + "Week: " + Double.toString(this.weeknumber) +"\n"
+                        + "Slot ID: " + Long.toString(this.slotid) + "\n"
+                        + "Contact: " + this.contact + "\n"
+                        + "Phone: " + this.phone + "\n";
+        return retval;
+    }
+
+    /**
+     * Get room.
+     * @param conn a connection to recover room via.
+     * @return the room.
+     */
+    public Room getRoom(UCLApiConnection conn) {
+        Hashtable<String, String> params = new Hashtable<String, String>();
+        params.put("siteid", this.siteid);
+        params.put("roomid", this.roomid);
+        return Room.searchAPI(conn, params)[0];
+    }
+
+    /**
+     * Set endtime.
+     * @param endtime the new endtime of the booking.
+     */
+    public void setEndtime(String endtime) {
+        this.endtime = new String(endtime);
+    }
+
+    /**
+     * Get end time.
+     * @return the end time of the booking.
+     */
+    public String getEndtime() {
+        return new String(this.endtime);
+    }
+
+    /**
+     * Set start time.
+     * @param starttime the new start time of the booking.
+     */
+    public void setStarttime(String starttime) {
+        this.starttime = new String(starttime);
+    }
+
+    /**
+     * Get start time.
+     * @return the start time of the booking.
+     */
+    public String getStarttime() {
+        return new String(this.starttime);
+    }
+
+    /**
+     * Set slot id.
+     * @param slotid the new id of the slot.
+     */
+    public void setSlotid(long slotid) {
+        this.slotid = slotid;
+    }
+
+    /**
+     * Get slot id.
+     * @return the slot id.
+     */
+    public long getSlotid() {
+        return this.slotid;
+    }
+
+    /**
+     * Set phone number.
+     * @param phone the new phone number
+     */
+    public void setPhone(String phone) {
+        this.phone = new String(phone);
+    }
+
+    /**
+     * Get phone number.
+     * @return the phone number.
+     */
+    public String getPhone() {
+        return new String(this.phone);
+    }
+
+    /**
+     * Set description.
+     * @param description the new description.
+     */
+    public void setDescription(String description) {
+        this.description = new String(description);
+    }
+
+    /**
+     * Get description.
+     * @return the description of the booking..
+     */
+    public String getDescription() {
+        return new String(this.description);
+    }
+
+
+    /**
+     * Set contact.
+     * @param contact the new contact for the booking.
+     */
+    public void setContact(String contact) {
+        this.contact = new String(contact);
+    }
+
+    /**
+     * Get contact.
+     * @return the contact for the booking.
+     */
+    public String getContact() {
+        return new String(this.contact);
+    }
+
+    /**
+     * Set week number.
+     * @param weeknumber the new weeknumber.
+     */
+    public void setWeeknumber(double weeknumber) {
+        this.weeknumber = weeknumber;
+    }
+
+    /**
+     * Get week number.
+     * @return the week number (a double).
+     */
+    public double getWeeknumber() {
+        return this.weeknumber;
+    }
+
+        /**
+     * Get room name.
+     * @return the name of the room.
+     */
+    public String getRoomname() {
+        return new String(this.roomname);
+    }
+
+    /**
+     * Set room id.
+     * @param id the new id of the room.
+     */
+    public void setRoomid(String id) {
+        this.roomid = new String(id);
+    }
+
+    /**
+     * Get room id.
+     * @return the id of the room.
+     */
+    public String getRoomid() {
+        return new String(this.roomid);
+    }
+
+    /**
+     * Set site id.
+     * @param id the new id of the site.
+     */
+    public void setSiteid(String id) {
+        this.siteid = new String(id);
+    }
+
+}
