51fa98a7b77390c67c1d1fe1c55609f7a748dc18
Owain Kenway <o.kenway@ucl.ac.uk>
2018-01-27 13:13:22 +0000

    Add methods for longs and doubles.

--- uclapi/JSONWrapper.java
+++ uclapi/JSONWrapper.java
@@ -15,11 +15,39 @@ public class JSONWrapper {
      * @param item the object.
      * @param field the field.
      */
-    public static String safeGet(JSONObject item, String field) {
+    public static String safeGetString(JSONObject item, String field) {
         try {
             return new String(((String)item.get(field)));
         } catch(NullPointerException e) {
             return new String("");
+        }
+    }
+
+    /**
+     * Use JSON.simple to get a value out of a JSONObject, returning 0 if
+     * the field is empty or does not exist.
+     * @param item the object.
+     * @param field the field.
+     */
+    public static long safeGetLong(JSONObject item, String field) {
+        try {
+            return ((long)item.get(field));
+        } catch(NullPointerException e) {
+            return 0;
+        }
+    }
+
+    /**
+     * Use JSON.simple to get a value out of a JSONObject, returning 0.0d if
+     * the field is empty or does not exist.
+     * @param item the object.
+     * @param field the field.
+     */
+    public static double safeGetDouble(JSONObject item, String field) {
+        try {
+            return  Double.parseDouble((String)item.get(field));
+        } catch(NullPointerException e) {
+            return 0.0d;
         }
     }
 
