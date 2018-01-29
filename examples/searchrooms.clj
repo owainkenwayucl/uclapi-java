824713563d1c9b960e64d8feab3c26522f0ccf29
Owain Kenway <o.kenway@ucl.ac.uk>
2018-01-29 12:00:43 +0000

    Search Rooms example in Clojure.

---
+++ examples/searchrooms.clj
@@ -0,0 +1,20 @@
+; Room Search example in Clojure
+; Owain Kenway
+
+; Make sure we have the right number of command line arguments.
+(if (= (count *command-line-args*) 2)
+  (def token (first *command-line-args*))
+  ((.println System/err "Run with API key and date as arguments.")(System/exit 1))
+)
+
+; Import necessary classes.
+(import uclapi.UCLApiConnection)
+(import uclapi.Room)
+(import java.util.Hashtable)
+
+; Put search parameters into a Hashtable.
+(def params (Hashtable.))
+(.put params "date" (second *command-line-args*))
+
+; Do our search and print results.
+(println (clojure.string/join "\n" (Room/searchAPI (UCLApiConnection. token) params)))
