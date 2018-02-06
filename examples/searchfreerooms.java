32f85d22f00263372a2e85553d3933b1f7bed771
Owain Kenway <o.kenway@ucl.ac.uk>
2018-02-06 15:44:41 +0000

    Java free rooms example.

---
+++ examples/searchfreerooms.java
@@ -0,0 +1,33 @@
+// UCL API example in Java - search for free rooms.
+// This example queries the roombookings/rooms endpoint and prints results.
+//
+// Owain Kenway
+
+package examples;
+
+import uclapi.UCLApiConnection;
+import uclapi.Room;
+
+import java.util.Hashtable;
+
+public class searchfreerooms {
+    public static void main(String[] args) {
+        if (args.length == 3) {
+
+            UCLApiConnection conn = new UCLApiConnection(args[0]);
+            Hashtable<String, String> params = new Hashtable<String, String>();
+            params.put("start_datetime", args[1]);
+            params.put("end_datetime", args[2]);
+
+            Room[] rooms = Room.searchAPIForFreeRooms(conn, params);
+
+            for (int i = 0; i<rooms.length; i++) {
+                System.out.println(rooms[i].toString());
+            }
+            
+        } else {
+            System.err.println("Run with API key + start + end datetimes as arguments.");
+            System.exit(1);
+        }
+    }
+}
