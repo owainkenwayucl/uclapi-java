317b919a1e94619d43493274e2850de1a852012c
Owain Kenway <o.kenway@ucl.ac.uk>
2018-02-07 12:56:35 +0000

    Jython example.

---
+++ examples/searchfreerooms.py
@@ -0,0 +1,27 @@
+# UCL API example in Jython
+# This example searches for free rooms.
+
+# Owain Kenway
+
+import uclapi.UCLApiConnection
+import uclapi.Room
+import java.util.Hashtable
+
+import sys
+
+# Get our key + query.
+if len(sys.argv) != 4:
+	sys.exit("Call with key and start/end dates as arguments")
+
+key = sys.argv[1]
+params = java.util.Hashtable()
+params.put("start_datetime", sys.argv[2])
+params.put("end_datetime", sys.argv[3])
+
+# Make our connection and get our results.
+conn = uclapi.UCLApiConnection(key)
+results = uclapi.Room.searchAPIForFreeRooms(conn, params)
+
+# Print them out.
+for a in results:
+	print(a.toString())
