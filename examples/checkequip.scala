eeffdfd03902f92e4d683726fb274148a4e5bc26
Owain Kenway <o.kenway@ucl.ac.uk>
2018-05-14 13:16:43 +0100

    Scala implementation of checkequip.

---
+++ examples/checkequip.scala
@@ -0,0 +1,32 @@
+// UCL API example in Java
+// This example searchs for equipment in a room.
+//
+// Owain Kenway
+
+package examples
+
+import uclapi.UCLApiConnection
+import uclapi.Equipment
+
+import java.util.Hashtable
+
+object checkequip extends App {
+
+    if (args.length != 3) {
+        Console.err.println("Run with API key + siteid + roomid as arguments.")
+        System.exit(1)
+    }
+
+    val apikey = args(0)
+    val params = new Hashtable[String, String]
+    params.put("roomid", args(2))
+    params.put("siteid", args(1))
+
+    val conn = new UCLApiConnection(apikey)
+
+    val results = Equipment.searchAPI(conn, params)
+
+    for (i <- 0 to (results.length - 1))
+        println(results(i).toString())
+
+}
