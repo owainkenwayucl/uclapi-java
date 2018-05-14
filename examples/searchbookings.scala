25a19bf7c5e9b54cb8443e41982b9d8af91c12b0
Owain Kenway <o.kenway@ucl.ac.uk>
2018-05-14 13:21:43 +0100

    Scala Booking search example.

---
+++ examples/searchbookings.scala
@@ -0,0 +1,31 @@
+// UCL API example in Java
+// This example queries the roombookings/rooms endpoint and prints results.
+//
+// Owain Kenway
+
+package examples
+
+import uclapi.UCLApiConnection
+import uclapi.Booking
+
+import java.util.Hashtable
+
+object searchbookings extends App {
+
+    if (args.length != 2) {
+        Console.err.println("Run with API key + siteid + roomid as arguments.")
+        System.exit(1)
+    }
+
+    val apikey = args(0)
+    val params = new Hashtable[String, String]
+    params.put("date", args(1))
+
+    val conn = new UCLApiConnection(apikey)
+
+    val results = Booking.searchAPI(conn, params)
+
+    for (i <- 0 to (results.length - 1))
+        println(results(i).toString())
+
+}
